66f0f58f2faa53d69383f76ff76f65042683e200
Noah Finberg <noahfinberg@gmail.com>
2015-05-19 22:17:36 +0000

    get sign up, sign in, and sign out functionality working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Nomster::Application.routes.draw do
+
+  root 'places#index'
+
+
   devise_for :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'places#index'
+  
 
   resources :places
 
